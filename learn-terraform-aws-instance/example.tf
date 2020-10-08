094784d6be9ede79a1bdf10d83ff748704e0f965
Dennis Henderson <dennismhenderson@gmail.com>
2020-10-08 09:02:13 -0400

    aws testing

---
+++ learn-terraform-aws-instance/example.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  # us-east-1 - Ubuntu Server 20.04 LTS (HVM), SSD Volume Type (64bit)
+  # ami           = "ami-0dba2cb6798deb6d8"
+  # us-east-1 - Ubuntu Server 18.04 LTS (HVM), SSD Volume Type (64bit)
+  ami           = "ami-0817d428a6fb68645"
+  instance_type = "t2.micro"
+}
--- setup.ps1
+++ setup.ps1
@@ -2,3 +2,8 @@
 choco install docker-desktop
 # The Windows Subsystem for Linux needs to be updated
 # https://docs.microsoft.com/en-us/windows/wsl/wsl2-kernel
+
+choco install awscli
+
+aws configure
+# Config and Credentials stored here: %UserProfile%\.aws\
